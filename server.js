9ad5a28652cd4bdc5b00672d51df2a4885ed7263
thara-messeroux <tharamesseroux@gmail.com>
2026-04-20 13:40:16 -0400

    setting up route patterns for RESTful APIs

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use(logger("dev"));
 
 // Routes go here
 
+/*
+
+HTTP Method	Controller	Response	URI	Use Case
+POST	create	200	/pets	Create a pet
+GET	index	200	/pets	List pets
+GET	show	200	/pets/:petId	Get a single pet
+PUT	update	200	/pets/:petId	Update a pet
+DELETE	deletePet	204	/pets/:petId	Delete a pet
+
+*/
+
+
 app.listen(3000, () => {
   console.log("The express app is ready!");
 });
