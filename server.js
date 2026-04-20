df6b5c8e51e9b8de38ebd126e7bd5e757a191f5e
thara-messeroux <tharamesseroux@gmail.com>
2026-04-20 13:06:38 -0400

    moved mongoose to its own file

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const logger = require('morgan');
+const logger = require("morgan");
 
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+require("./db/connection");
 
 app.use(express.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Routes go here
 
 app.listen(3000, () => {
-  console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
